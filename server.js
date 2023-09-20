5daa7a2a199998a490b4960c28d37a2fc0528417
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2023-09-20 10:34:44 -0400

    added root

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get("/goodbye", (req, res) => {
   res.send("Sayonara, World!");
 });
 
+app.get("/", (req, res) => {
+  res.send("<H1>THIS IS AN API</H1>");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
